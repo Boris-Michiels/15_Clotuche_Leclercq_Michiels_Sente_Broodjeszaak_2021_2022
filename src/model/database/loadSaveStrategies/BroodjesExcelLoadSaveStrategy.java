3a68d9d3ff0db186108f79cf0336f4b0ba37b588
TomSente <tom.sente@student.ucll.be>
2021-12-01 16:43:12 +0100

    stap 4 klaar story 2

--- src/model/database/loadSaveStrategies/BroodjesExcelLoadSaveStrategy.java
+++ src/model/database/loadSaveStrategies/BroodjesExcelLoadSaveStrategy.java
@@ -1,9 +1,18 @@
 package model.database.loadSaveStrategies;
 
+import model.Broodje;
 import utilities.ExcelLoadSaveTemplate;
 
 public class BroodjesExcelLoadSaveStrategy extends ExcelLoadSaveTemplate {
 
 
+    @Override
+    public Object getKey(String[] s) {
+        return s[0];
+    }
 
+    @Override
+    public Object makeObject(String[] s) {
+        return new Broodje(s[0], Double.parseDouble(s[1]), Integer.parseInt(s[2]), Integer.parseInt(s[3]));
+    }
 }
--- src/utilities/ExcelLoadSaveTemplate.java
+++ src/utilities/ExcelLoadSaveTemplate.java
@@ -2,11 +2,15 @@ package utilities;
 
 import excel.ExcelPlugin;
 import jxl.read.biff.BiffException;
+import model.Product;
 
+import javax.swing.*;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public abstract class ExcelLoadSaveTemplate <K,V>  {
@@ -14,17 +18,39 @@ public abstract class ExcelLoadSaveTemplate <K,V>  {
 
     public final Map<K,V> load(String path) {
         Map<K,V> objects = new HashMap<>();
-
         try
         {
             File file = new File(path);
             ArrayList<ArrayList<String>> result = excelPlugin.read(file);
-
+            for(ArrayList<String> a : result)
+            {
+                String[] dataSet = a.toArray(new String[0]);
+                K key = getKey(dataSet);
+                V object = makeObject(dataSet);
+                objects.put(key,object);
+            }
         }
         catch (BiffException | IOException i) {
             System.out.println(i);
         }
-        return
+        return objects;
+    }
+
+    public final void save(String path, Map<K,V> objects)
+    { try
+        {
+            File file = new File(path);
+            FileWriter fileWriter = new FileWriter(file);
+            List<V> products = (ArrayList<V>) objects.values();
+            for (V v : products)
+            {
+                fileWriter.write(((Product) v).toTxtLine());
+            }
+        }
+        catch(IOException e)
+        {
+            System.out.println(e);
+        }
     }
 
     public abstract K getKey(String[] s);
